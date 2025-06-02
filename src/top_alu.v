1749d4ca0685c6d46c8b2562d7aeb9c5d3740c30
LindaPantaleon <lpantaleond1@miumg.edu.gt>
2025-06-02 13:16:42 -0600

    Update top_alu.v

--- src/top_alu.v
+++ src/top_alu.v
@@ -120,36 +120,51 @@ module Prefix_adder(
     output       Cout
 );
 
-    wire [7:0] G, P, X;
+    // Generate y Propagate
+    wire [7:0] G, P;
+    wire [7:0] X;
     wire [8:0] C;
-
-    wire [7:0] G1, P1, G2, P2, G3, P3;
 
     assign X = A ^ B;
     assign G = A & B;
     assign P = A | B;
 
-    assign G1[0] = G[0]; assign P1[0] = P[0];
-    assign G1[1] = G[1]; assign P1[1] = P[1];
-    assign G1[2] = G[2] | (P[2] & G[1]); assign P1[2] = P[2] & P[1];
-    assign G1[3] = G[3]; assign P1[3] = P[3];
-    assign G1[4] = G[4] | (P[4] & G[3]); assign P1[4] = P[4] & P[3];
-    assign G1[5] = G[5]; assign P1[5] = P[5];
-    assign G1[6] = G[6] | (P[6] & G[5]); assign P1[6] = P[6] & P[5];
-    assign G1[7] = G[7]; assign P1[7] = P[7];
+    // Etapas intermedias
+    wire [7:0] G1, P1;
+    wire [7:0] G2, P2;
+    wire [7:0] G3, P3;
 
-    assign G2[0] = G1[0]; assign P2[0] = P1[0];
-    assign G2[1] = G1[1]; assign P2[1] = P1[1];
-    assign G2[2] = G1[2]; assign P2[2] = P1[2];
+    // Nivel 1
+    assign G1[0] = G[0];             assign P1[0] = P[0];
+    assign G1[1] = G[1];             assign P1[1] = P[1];
+    assign G1[2] = G[2] | (P[2] & G[1]);  assign P1[2] = P[2] & P[1];
+    assign G1[3] = G[3];             assign P1[3] = P[3];
+    assign G1[4] = G[4] | (P[4] & G[3]);  assign P1[4] = P[4] & P[3];
+    assign G1[5] = G[5];             assign P1[5] = P[5];
+    assign G1[6] = G[6] | (P[6] & G[5]);  assign P1[6] = P[6] & P[5];
+    assign G1[7] = G[7];             assign P1[7] = P[7];
+
+    // Nivel 2
+    assign G2[0] = G1[0];                 assign P2[0] = P1[0];
+    assign G2[1] = G1[1];                 assign P2[1] = P1[1];
+    assign G2[2] = G1[2];                 assign P2[2] = P1[2];
     assign G2[3] = G1[3] | (P1[3] & G1[2]); assign P2[3] = P1[3] & P1[2];
-    assign G2[4] = G1[4]; assign P2[4] = P1[4];
+    assign G2[4] = G1[4];                 assign P2[4] = P1[4];
     assign G2[5] = G1[5] | (P1[5] & G1[4]); assign P2[5] = P1[5] & P1[4];
-    assign G2[6] = G1[6]; assign P2[6] = P1[6];
+    assign G2[6] = G1[6];                 assign P2[6] = P1[6];
     assign G2[7] = G1[7] | (P1[7] & G1[6]); assign P2[7] = P1[7] & P1[6];
 
-    assign G3 = G2;
-    assign P3 = P2;
+    // Nivel 3
+    assign G3[0] = G2[0]; assign P3[0] = P2[0];
+    assign G3[1] = G2[1]; assign P3[1] = P2[1];
+    assign G3[2] = G2[2]; assign P3[2] = P2[2];
+    assign G3[3] = G2[3]; assign P3[3] = P2[3];
+    assign G3[4] = G2[4]; assign P3[4] = P2[4];
+    assign G3[5] = G2[5]; assign P3[5] = P2[5];
+    assign G3[6] = G2[6]; assign P3[6] = P2[6];
+    assign G3[7] = G2[7]; assign P3[7] = P2[7];
 
+    // Cálculo de carry
     assign C[0] = Cin;
     assign C[1] = G[0] | (P[0] & C[0]);
     assign C[2] = G1[1] | (P1[1] & C[1]);
@@ -160,6 +175,7 @@ module Prefix_adder(
     assign C[7] = G2[6] | (P2[6] & C[6]);
     assign C[8] = G2[7] | (P2[7] & C[7]);
 
+    // Suma
     assign S = X ^ C[7:0];
     assign Cout = C[8];
 
